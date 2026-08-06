962d0b7c3bf75ac07b1fdc4b2985299e8490ff02
Tauriq <tauriqp88@gmail.com>
2026-08-06 08:19:25 +0200

    Copied the args check if statement and the printUsage method from FireLineSerial.java

--- src/FireLineParallel.java
+++ src/FireLineParallel.java
@@ -10,9 +10,18 @@ public class FireLineParallel extends RecursiveTask<Integer>{
     private static final int CUTOFF = 50;
     private int i;
 
-    public FireLineParallel(int i) {
-        this.i = i;
+    private int[] grid;
+    private int start;
+    private int end;
+
+
+    public FireLineParallel(int[] grid, int start, int end) {
+        this.grid = grid;
+        this.start = start;
+        this.end = end;
     }
+
+    
 
     public void run() {
 
@@ -20,9 +29,28 @@ public class FireLineParallel extends RecursiveTask<Integer>{
     }
 
     public static void main(String[] args) {
+         if (args.length < 5 || args.length > 11 || (args.length > 8 && args.length < 11)) {
+            printUsage();
+            System.exit(1);
+        }
 
 
+    }
 
+    
+    private static void printUsage() {
+        System.err.println(
+                "Usage: java FirelineSerial <rows> <columns> <seed> "
+                + "<diffusion|wildfire> <output-prefix> "
+                + "[max-steps] [tolerance] [mixed|grass] "
+                + "[ignition-top-row ignition-left-column patch-size]");
+        System.err.println("Examples:");
+        System.err.println(
+                "  java FirelineSerial 300 300 42 wildfire "
+                + "output/fireline");
+        System.err.println(
+                "  java FirelineSerial 2000 2000 17 wildfire "
+                + "output/benchmark 50000 0.05 grass 20 20 9");
     }
 
     
